b6068dc113ed497969c9f684c4004705e7c77aa2
Alan Lam <all123@its-pch264-22.ucsd.edu>
2019-02-03 16:07:02 -0800

    Initial Commit

---
+++ Touch Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touch Counter
+//
+//  Created by Alan Lam on 2/3/19.
+//  Copyright © 2019 Alan Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
