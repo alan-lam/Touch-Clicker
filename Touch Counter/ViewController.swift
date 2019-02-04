395a84e23ef609b059da440fb145601bc62bc373
Alan Lam <all123@its-pch264-22.ucsd.edu>
2019-02-03 16:29:22 -0800

    completed functionality

--- Touch Counter/ViewController.swift
+++ Touch Counter/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var colorFlag = 0
+    var touchCounter = 0
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBAction func colorButton(_ sender: Any) {
+        if colorFlag == 0 {
+            self.view.backgroundColor = .black
+            countLabel.textColor = .green
+            titleLabel.textColor = .green
+            colorFlag = 1
+        }
+        else {
+            self.view.backgroundColor = .white
+            countLabel.textColor = .black
+            titleLabel.textColor = .black
+            colorFlag = 0
+        }
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        touchCounter = 0
+        countLabel.text = "Count = \(touchCounter)"
+    }
+    
+    @IBAction func touchButton(_ sender: Any) {
+        touchCounter += 1
+        countLabel.text = "Count = \(touchCounter)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
